c871cdbf729f81403893e8c213e1b301cbb8bf56
thoufeeque0007 <101185197+thoufiq0786@users.noreply.github.com>
2022-03-09 21:43:04 +0530

    finalyze

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class ContactComponent implements OnInit {
 
   constructor() {
-    // console.warn("consthfhfgructor")
+    console.warn("consthfhfgructor")
    }
 
   ngOnInit(): void {
